e6ab558c6eeba25929a73ec03a451fb8f266a0ce
Neeraj Reddy <neeraj@rapidinnovation.dev>
2022-08-31 12:28:26 +0530

    initial push

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x3bdAbD15934b371F9D41FC45624B278064F7bAA9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
