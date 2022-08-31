53fb7ba45f4b5420133d5ac6dd79e59cdb31ecd4
Neeraj Reddy <neeraj@rapidinnovation.dev>
2022-08-31 14:43:40 +0530

    initial push

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config({ path: ".env" });
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+const API_KEY = process.env.API_KEY;
 
 module.exports = {
   solidity: "0.8.4",
@@ -13,4 +16,9 @@ module.exports = {
       accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: {
+      rinkeby: "MBFZG56DZY25A7YPX8ATV9QUYX8XRVDTE4"
+    }
+  }  
 };
